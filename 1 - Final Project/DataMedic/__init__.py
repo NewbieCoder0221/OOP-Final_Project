2e4fb0b78e3ff9453ac24ec50ea42257dd8a3247
Bulalo_Coder <javemelchorodchigue2006@gmail.com>
2025-11-14 13:11:06 +0800

    Update __init__.py

--- 1 - Final Project/DataMedic/__init__.py
+++ 1 - Final Project/DataMedic/__init__.py
@@ -1,8 +1,9 @@
-from .inspector import DataInspector
-from .doctor import DataDoctor
-from .report import ReportGenerator
+from .data_inspector import DataInspector
+from .data_doctor import DataDoctor
+from .report_generator import ReportGenerator
 
 __all__ = ["DataInspector", "DataDoctor", "ReportGenerator"]
+
 
 __version__ = "0.1.0"
 __author__ = "Your Group Name"
